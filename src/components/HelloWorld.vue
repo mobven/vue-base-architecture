6907bcc7626c5e381e3f5fbbee6ca75b026d7388
Serhan Ertürk <srnerturk@gmail.com>
2023-02-21 19:40:19 +0300

    error fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,30 +26,39 @@ export default defineComponent({
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div>
+      <p>
+        Recommended IDE setup:
+        <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+        +
+        <a href="https://github.com/johnsoncodehk/volar" target="_blank"
+          >Volar</a
+        >
+      </p>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+      <p>See <code>README.md</code> for more information.</p>
 
-  <p>See <code>README.md</code> for more information.</p>
+      <p>
+        <a href="https://vitejs.dev/guide/features.html" target="_blank">
+          Vite Docs
+        </a>
+        |
+        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+      </p>
+    </div>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <div>
+      <button type="button" @click="count++">count is: {{ count }}</button>
+    </div>
+    <div>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test hot module replacement.
+      </p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
