2fe3df72785284cb93ac47bb9ae1c3774364efa3
Serhan Ertürk <srnerturk@gmail.com>
2023-02-21 15:27:50 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Hello Vue 2 + TypeScript + Vite" />
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { createVuePlugin } from "vite-plugin-vue2";
+
+const path = require("path");
+
+export default defineConfig({
+  plugins: [createVuePlugin()],
+  resolve: {
+    alias: [
+      {
+        find: /^@\/(.+)/,
+        replacement: path.resolve(path.resolve(__dirname), "src") + "/$1",
+      },
+    ],
+  },
+});
