ac60b5b3c8ce9d684b615bed62544a2f2902f8c7
Serhan Ertürk <srnerturk@gmail.com>
2023-02-21 19:58:06 +0300

    added sample button

---
+++ src/components/Button.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { PropType } from "vue";
+interface Props {
+  text: string;
+  type: string;
+  disabled: boolean;
+  onClick: () => void;
+}
+
+export default defineComponent({
+  name: "ButtonComponent",
+  props: {
+    text: {
+      type: String as PropType<Props["text"]>,
+      required: true,
+    },
+    type: {
+      type: String as PropType<Props["type"]>,
+      required: true,
+    },
+    disabled: {
+      type: Boolean as PropType<Props["disabled"]>,
+      required: true,
+    },
+    onClick: {
+      type: Function as PropType<Props["onClick"]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <button type="button" :class="type" :disabled="disabled" @click="onClick">
+    {{ text }}
+  </button>
+</template>
