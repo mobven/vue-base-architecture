db33d29fd33e317c334467c923323c8798aa6425
Serhan Ertürk <srnerturk@gmail.com>
2023-02-21 19:48:38 +0300

    axios util refactored

--- src/services/TodoService.ts
+++ src/services/TodoService.ts
@@ -1,4 +1,4 @@
-import axiosUtil from "@/utils/axios-util";
+import axios from "@/utils/axios-util";
 import { Todo } from "../models/Todo";
 
 interface ITodoService {
@@ -7,7 +7,7 @@ interface ITodoService {
 
 export default class TodoService implements ITodoService {
   public async getTodos(): Promise<Todo[]> {
-    const response = await axiosUtil.get("/todos");
+    const response = await axios.get("/todos");
     const todos: Todo[] = response.data;
     return todos;
   }
--- src/utils/axios-util.ts
+++ src/utils/axios-util.ts
@@ -1,34 +1,36 @@
-import axios, { InternalAxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { InternalAxiosRequestConfig, AxiosError } from "axios";
 
-const axiosUtil = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+axios.defaults.baseURL = import.meta.env.VITE_API_URL as string;
 
 // Add a request interceptor
-axiosUtil.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
-    // Do something before request is sent
-    return config;
-  },
-  (error) => {
-    // Do something with request error
+axios.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  // Do something before request is sent
+  return config;
+});
+
+axios.interceptors.response.use(
+  (res) => res,
+  (error: AxiosError) => {
+    const { data, status } = error.response!;
+    switch (status) {
+      case 400:
+        console.error(data);
+        break;
+
+      case 401:
+        console.error("unauthorised");
+        break;
+
+      case 404:
+        console.error("/not-found");
+        break;
+
+      case 500:
+        console.error("/server-error");
+        break;
+    }
     return Promise.reject(error);
   }
 );
 
-// Add a response interceptor
-axiosUtil.interceptors.response.use(
-  (response: AxiosResponse) => {
-    // Do something with response data
-    return response;
-  },
-  (error) => {
-    // Do something with response error
-    return Promise.reject(error);
-  }
-);
-
-export default axiosUtil;
+export default axios;
