0a1f1e6c5c15c23dec1adb1cc92e72a284790edc
Serhan Ertürk <srnerturk@gmail.com>
2023-02-22 11:52:30 +0300

    feat: removed action interfaces

--- src/store/theme/actions.ts
+++ src/store/theme/actions.ts
@@ -1,24 +1,9 @@
 import type { ActionTree, ActionContext } from "vuex";
 import type { State } from "./state";
-import type { Mutations } from "./mutations";
 import { MutationMethods } from "./mutations";
-
-export type ActionCtx = {
-  commit<K extends keyof Mutations>(
-    key: K,
-    payload: Parameters<Mutations[K]>[1]
-  ): ReturnType<Mutations[K]>;
-} & Omit<ActionContext<State, State>, "commit">;
 
 export enum ActionMethods {
   changeTheme = "changeTheme",
-}
-
-export interface Actions {
-  [ActionMethods.changeTheme](
-    ctx: ActionCtx,
-    theme: "light" | "dark"
-  ): Promise<void>;
 }
 
 export const actions: ActionTree<State, State> = {
--- src/store/todo/actions.ts
+++ src/store/todo/actions.ts
@@ -1,25 +1,13 @@
-import type { ActionTree, ActionContext } from "vuex";
+import type { ActionTree } from "vuex";
 import type { State } from "./state";
-import type { Mutations } from "./mutations";
 import { MutationMethods } from "./mutations";
 import TodoService from "@/services/TodoService";
-
-export type ActionCtx = {
-  commit<K extends keyof Mutations>(
-    key: K,
-    payload: Parameters<Mutations[K]>[1]
-  ): ReturnType<Mutations[K]>;
-} & Omit<ActionContext<State, State>, "commit">;
 
 export enum ActionMethods {
   fetchTodos = "fetchTodos",
 }
 
-export interface Actions {
-  [ActionMethods.fetchTodos](ctx: ActionCtx): Promise<void>;
-}
-
-export const actions: ActionTree<State, State> & Actions = {
+export const actions: ActionTree<State, State> = {
   async [ActionMethods.fetchTodos]({ state, commit }) {
     const service = new TodoService();
     const result = await service.getTodos();
